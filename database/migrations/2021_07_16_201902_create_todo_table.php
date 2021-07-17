50c05ddfc6ec6dcc13c183f9a5f9380bf5292cd0
ftufanovixve <francesco.tufano@vixve.us>
2021-07-17 07:01:08 -0400

    initial commit

---
+++ database/migrations/2021_07_16_201902_create_todo_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTodoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todo', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->tinyInteger('status');
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('folder_id')->nullable();
+
+            $table->index('users_id','fk_todo_users1_idx');
+		    $table->index('folder_id','fk_todo_folder1_idx');
+		
+		    $table->foreign('users_id')
+		        ->references('id')->on('users');
+		
+		    $table->foreign('folder_id')
+		        ->references('id')->on('folder');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todo');
+    }
+}
